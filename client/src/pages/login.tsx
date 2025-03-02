64c848a072d470a767b5ce106c1f81496fd4d884
John Kristan Torremocha <johnkristan01@gmail.com>
2025-03-02 21:54:27 +0800

    feat: styled the landing page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -6,19 +6,19 @@ export default function LoginPage(){
     return(
         <div className="h-screen flex justify-center items-center font-bold">
 
-            <div className="bg-green-500 rounded-md p-6 w-[35%] h-[70%] flex flex-col items-center justify-center text-white gap-5">
-                <div className="flex justify-center items-center gap-2">
+            <div className="bg-green-500 rounded-md p-6 w-[35%] h-[70%] flex flex-col items-center text-white gap-5">
+                <div className="flex flex-col justify-center items-center gap-2 mt-12">
                     <img 
                         src="/img/city_logo.jpg" 
                         alt="City Agriculture Logo" 
-                        className="rounded-full w-[20%]"
+                        className="rounded-full w-[40%]"
                     />
 
                     <h1 className="text-3xl">Welcome to Fruitection</h1>
 
                 </div>
 
-                <h1 className="text-sm text-center">Stay informed with the latest statistics on diseases affecting durian crops. Get crucial data to support farmers and researchers in tackling health challenges in durian cultivation.</h1>
+                <h1 className="text-sm text-center opacity-75">Stay informed with the latest statistics on diseases affecting durian crops. Get crucial data to support farmers and researchers in tackling health challenges in durian cultivation.</h1>
 
             </div>
 
